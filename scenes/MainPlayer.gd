71f3bb1a0b59e76efbbdb4d77dd23598ed85e36e
Miguel Menjivar <miguel@b01-aruba1-guest-10-110-83-244.wlan.cpp.edu>
2019-04-22 19:33:40 -0700

    made the platforms and jumping all work now

---
+++ scenes/MainPlayer.gd
@@ -0,0 +1,31 @@
+extends RigidBody2D
+
+var jump_speed = 600 # how high character will jump
+var speed = 300 # how fast movement left to right
+
+#var sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#sprite = get_node("Sprite") # uncomment when we get the sprite to work
+	set_physics_process(true) # set_fixed_process(true)
+	pass
+
+func _physics_process(delta): # _fixed_process
+	var left_key = Input.is_action_pressed("ui_left") 
+	var right_key = Input.is_action_pressed("ui_right")
+
+	if left_key: # when left key presssed, move left
+		set_linear_velocity(Vector2(-speed, get_linear_velocity().y))
+# 		sprite.set_flip_h(true) # when we get main sprite, we can make character face direction of movement
+	if right_key: # when right key pressed, move right
+		set_linear_velocity(Vector2(speed, get_linear_velocity().y))
+#		sprite.set_flip_h(false) # when we get main sprite, we can make character face direction of movement
+	if !left_key and !right_key: # when none of keys are pressed
+		set_linear_velocity(Vector2(0, get_linear_velocity().y))
+	
+# will jump once hits a platform, like doodle jump. will never be at rest
+func _on_Area2D_body_entered(body): #collision yt
+	if body.is_in_group('Paddles') and get_linear_velocity().y >= 0:
+		set_linear_velocity(Vector2(0,-jump_speed))
+	pass # Replace with function body.
