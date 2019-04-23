0f84acd152c7d5719e55fc6ebbf09951e318db3c
Chy Lim <cirshin@gmail.com>
2019-04-23 15:12:45 -0700

    Character faces direction of movement

--- scenes/MainPlayer.gd
+++ scenes/MainPlayer.gd
@@ -3,11 +3,11 @@ extends RigidBody2D
 var jump_speed = 600 # how high character will jump
 var speed = 300 # how fast movement left to right
 
-#var sprite
+var sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#sprite = get_node("Sprite") # uncomment when we get the sprite to work
+	sprite = get_node("Sprite") # uncomment when we get the sprite to work
 	set_physics_process(true) # set_fixed_process(true)
 	pass
 
@@ -17,9 +17,11 @@ func _physics_process(delta): # _fixed_process
 
 	if left_key: # when left key presssed, move left
 		set_linear_velocity(Vector2(-speed, get_linear_velocity().y))
+		$Sprite.flip_h = false  # character faces left
 # 		sprite.set_flip_h(true) # when we get main sprite, we can make character face direction of movement
 	if right_key: # when right key pressed, move right
 		set_linear_velocity(Vector2(speed, get_linear_velocity().y))
+		$Sprite.flip_h = true    # character faces right
 #		sprite.set_flip_h(false) # when we get main sprite, we can make character face direction of movement
 	if !left_key and !right_key: # when none of keys are pressed
 		set_linear_velocity(Vector2(0, get_linear_velocity().y))
