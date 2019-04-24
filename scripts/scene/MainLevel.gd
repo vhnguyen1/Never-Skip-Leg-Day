5278cebc30c07663379696068878ca33f9de0fc5
Chy Lim <cirshin@gmail.com>
2019-04-24 02:30:26 -0700

    ?

--- scripts/scene/MainLevel.gd
+++ scripts/scene/MainLevel.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
 var platform = preload('res://scenes/mainPlatform.tscn')
+var break_platform = preload('res://scenes/break_platform.tscn')
 var width 
 
 func _ready():
 	width = get_viewport_rect().size.x
-	#width = get_viewport().get_rect().size.x
 	var y = 0
-	while y > -3000:
+	while y > -30000:
 		var new_platform = platform.instance()
-		new_platform.set_position(Vector2(rand_range(-width/2,width/2),y))
+		var cloud = break_platform.instance()
+		new_platform.set_position(Vector2(rand_range(-width/4,width/4),y))
+		cloud.set_position(Vector2(rand_range(-width/3,width/3),y))
 		add_child(new_platform)
-		y -= rand_range(0,210)
+		add_child(cloud)
+		y -= rand_range(0,180)
 	pass # Replace with function body.
+
+	
