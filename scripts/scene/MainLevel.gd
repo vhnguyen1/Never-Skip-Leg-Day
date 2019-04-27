5a6c61449c89916ec2f8d273e8547a65dda44a8c
Chy Lim <cirshin@gmail.com>
2019-04-27 16:43:00 -0700

    test

--- scripts/scene/MainLevel.gd
+++ scripts/scene/MainLevel.gd
@@ -1,18 +1,30 @@
 extends Node2D
 
 var platform = preload('res://objects/platform/Platform.tscn')
-var break_platform = preload('res://scenes/break_platform.tscn')
+var platform_extra = preload('res://objects/platform/Platform2.tscn')
+#var break_platform = preload('res://scenes/break_platform.tscn')
 var width 
 
+# game window is (1280 x 720)
 func _ready():
 	width = get_viewport_rect().size.x
+	var x = -200 #initial platform under character
 	var y = 0
+	var y2 = 200
+	
+	var possible_platform #platforms that can always be jumped to
+	var platform2  #extra platforms for variety, not always possible to jump to
 	while y > -30000:
-		var new_platform = platform.instance()
-		var cloud = break_platform.instance()
-		new_platform.set_position(Vector2(rand_range(-width/4,width/4),y))
-		cloud.set_position(Vector2(rand_range(-width/3,width/3),y))
-		add_child(new_platform)
-		add_child(cloud)
-		y -= rand_range(0,180)
+		possible_platform = platform.instance()
+		platform2 = platform_extra.instance()
+		possible_platform.set_position(Vector2(x,y))
+		platform2.set_position(Vector2(rand_range(-width + 50, width - 50),y2))
+		add_child(possible_platform)
+		add_child(platform2)
+		x = rand_range(x-400,x+400) # max horizontal travel distance somewhere around 400
+		while(x < (-width/2)+100 or x > (width/2)-100):
+			x = rand_range(x-400,x+400)
+		y -= rand_range(50,220) # max jump is ~230 right now
+		y2 -= rand_range(400, 800)
+		
 	pass # Replace with function body.
