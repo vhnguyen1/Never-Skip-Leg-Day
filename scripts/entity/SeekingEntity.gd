e959e509fb34f2ecf5ac14ea294c62d4a8ea3d45
Vincent Nguyen <21348834+vhnguyen1@users.noreply.github.com>
2019-04-28 12:34:03 -0700

    Added new enemies which include a Space Robot, Alien, Tundra Yeti and a Tundra Wolf. Also linked them to the proper scripts and added the necessary assets.

--- scripts/entity/SeekingEntity.gd
+++ scripts/entity/SeekingEntity.gd
@@ -8,16 +8,17 @@ var direction = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	enemy_to_player = player.translation - self.translation
+#	enemy_to_player = player.translation - self.translation
 	
-	if enemy_to_player.length() < fov_range:
-		if acos(enemy_to_player.normalized().dot(direction)) < FOV:
-			#Next step awaits.
-			$DetectionRay.cast_to = enemy_to_player
-			if $RayCast2D.is_colliding():
-				if $RayCast2D.get_collider() == Player:
-					#Do what ever you do when a player is detected
-					pass
+#	if enemy_to_player.length() < fov_range:
+#		if acos(enemy_to_player.normalized().dot(direction)) < FOV:
+#			#Next step awaits.
+#			$DetectionRay.cast_to = enemy_to_player
+#			if $RayCast2D.is_colliding():
+#				if $RayCast2D.get_collider() == Player:
+#					#Do what ever you do when a player is detected
+#					pass
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
