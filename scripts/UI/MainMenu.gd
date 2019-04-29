4362d12781334d267adc2384f01208e01080d4dc
Vincent Nguyen <21348834+vhnguyen1@users.noreply.github.com>
2019-04-29 14:14:01 -0700

    Compiled all branches onto master. THIS IS NOT ORGANIZED AND WILL NOT BE READY TO PULL. Please wait until I finish and inform everybody!

--- scripts/UI/MainMenu.gd
+++ scripts/UI/MainMenu.gd
@@ -1,19 +1,36 @@
 extends MarginContainer
 
-const start_game_path = "res://scenes/TestScene.tscn"
+#Commented out because the playbutton as of this time only changes the scene where the anumbis guy is swinging scepter
+#--------------------------------------------------------#
 
+#const start_game_path = "res://scenes/TestScene.tscn" 
+
+#-------------------------------------------------------------#
+
+var paused = true;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/MenuOptions/PlayButton.connect("pressed", self, "_load_game")
 	$VBoxContainer/MenuOptions/SettingsButton.connect("pressed", self, "_load_options")
 	$VBoxContainer/MenuOptions/QuitButton.connect("pressed", self, "_quit")
-	#Music.play()
+	$VBoxContainer/MenuOptions/Pause.connect("pressed", self, "_pause")
 func _process(delta):
 	pass
 	
 # Plays the game
+##################Change the scene to the one that you guys are working on###################
 func _load_game():
-	get_tree().change_scene(start_game_path)
+	#get_tree().change_scene(start_game_path)
+	pass
+	
+#pauses game
+func _pause():
+	if(paused == true):
+		get_tree().paused = paused
+		paused = false
+	elif(paused == false):
+		get_tree().paused = paused
+		paused = true
 
 # Opens options probably for instructions on how to play
 func _load_options():
--- scripts/UI/SettingMenu.gd
+++ scripts/UI/SettingMenu.gd
@@ -1,25 +1,26 @@
 extends MarginContainer
 
 const start_game_path = "res://scenes/MainMenu.tscn"
-
+var clicked = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/MenuOptions/BackButton.connect("pressed", self, "_main_menu")
-	$VBoxContainer/MenuOptions/SettingsButton.connect("pressed", self, "_load_options")
-	$VBoxContainer/MenuOptions/QuitButton.connect("pressed", self, "_quit")
-	#Music.play()
+	$VBoxContainer/MenuOptions/FullscreenButton.connect("pressed", self, "_load_fullscreen")
+	$VBoxContainer/MenuOptions/MusicButton.connect("pressed", self, "_music")
 func _process(delta):
 	pass
-
-# Plays the game
+# Goes back to main menu
 func _main_menu():
 	get_tree().change_scene(start_game_path)
 
-# Opens options probably for instructions on how to play
-func _load_options():
-	#get_tree().change_scene("res://scenes/Setting.tscn")
-	pass
-
-# quits game || closes the game
-func _quit():
-	get_tree().quit()
+# enables/disables fullscreen
+func _load_fullscreen():
+ 	   OS.window_fullscreen = !OS.window_fullscreen
+# starts stops main music
+func _music():
+	if( clicked == true):
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+		clicked = false
+	else:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+		clicked = true
