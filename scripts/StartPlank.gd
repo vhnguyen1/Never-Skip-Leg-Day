4b646593d5e2af1c5e7a59d42eb85491d1369dd2
Rene B. Dena <renedena842@gmail.com>
2019-05-02 21:00:38 -0700

    Update StartPlank.gd

--- scripts/StartPlank.gd
+++ scripts/StartPlank.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+const PLANK_TIME_CAP = 7
+
 var timer = 0
 
 func _physics_process(delta):
@@ -7,6 +9,6 @@ func _physics_process(delta):
 	timer += delta
 	position.y += delta * 25
 	#************teleport offscreen************
-	if timer > 7:
-		global_position.x=1000
-		global_position.y=1000
+	if timer > PLANK_TIME_CAP:
+		global_position.x = 1000
+		global_position.y = 1000
