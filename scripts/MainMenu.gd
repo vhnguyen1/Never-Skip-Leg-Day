89c936f52ea707bebae42f8e106a8dfd5f03f1b7
Vincent Nguyen <21348834+vhnguyen1@users.noreply.github.com>
2019-05-07 16:47:15 -0700

    Added UI button sounds on clicking.

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -13,6 +13,7 @@ extends MarginContainer
 const STAGE_PATH = "res://scenes/stages/"
 const START_GAME_PATH = STAGE_PATH + "SkyLevel.tscn"
 
+const BUTTON_SFX_PLAYER_PATH = "../ButtonSFXPlayer"
 const SETTINGS_PATH = "../SettingsMenu"
 const AVATAR_SPRITE_PATH = "../Avatar"
 const CLICK_TO_START_PATH = "../ClickText"
@@ -35,6 +36,7 @@ var paused
 var play_button
 var settings_button
 var quit_button
+var button_sfx_player
 var is_displaying_splashscreen
 
 #------------------------- Resources ------------------------------------#
@@ -52,6 +54,7 @@ func _ready():
 	settings_menu = get_node(SETTINGS_PATH)
 	avatar_sprite = get_node(AVATAR_SPRITE_PATH)
 	click_text = get_node(CLICK_TO_START_PATH)
+	button_sfx_player = get_node(BUTTON_SFX_PLAYER_PATH)
 	
 	# Link play button to the level scenes
 	play_button = get_node(VBOX_MENU_OPTIONS_PLAY_BUTTON_PATH)
@@ -70,14 +73,19 @@ func _ready():
 	#$VBoxContainer/MenuOptions/QuitButton.connect(PRESSED_CODE, self, QUIT_FUNCTION_NAME)
 	#$VBoxContainer/MenuOptions/Pause.connect(PRESSED_CODE, self, PAUSE_GAME_FUNCTION_NAME)
 
+func _play_button_sound():
+	button_sfx_player.play(0.0)
+
 func _input(event):
 	if is_displaying_splashscreen and event.is_pressed() and event.button_index == BUTTON_LEFT:
+		_play_button_sound()
 		self.visible = true
 		avatar_sprite.visible = true
 		click_text.visible = false
 		get_tree().set_input_as_handled()
 		is_displaying_splashscreen = false
 	elif !is_displaying_splashscreen and event is InputEventKey and event.is_pressed() and event.scancode == KEY_ESCAPE:
+		_play_button_sound()
 		self.visible = false
 		avatar_sprite.visible = false
 		click_text.visible = true
@@ -86,12 +94,14 @@ func _input(event):
 
 # Plays the game
 func _load_game():
+	_play_button_sound()
 	self.visible = false
 	avatar_sprite.visible = false
 	get_tree().change_scene(START_GAME_PATH)
 
 # Opens options probably for instructions on how to play
 func _load_options():
+	_play_button_sound()
 	self.visible = false
 	click_text.visible = false
 	avatar_sprite.visible = false
@@ -100,6 +110,7 @@ func _load_options():
 
 # Quits game || closes the game
 func _quit():
+	_play_button_sound()
 	get_tree().quit()
 
 # Pauses the game
--- scripts/SettingMenu.gd
+++ scripts/SettingMenu.gd
@@ -1,5 +1,6 @@
 extends MarginContainer
 
+const BUTTON_SFX_PLAYER_PATH = "../ButtonSFXPlayer"
 const MAIN_MENU_PATH = "../MainMenu"
 const AVATAR_SPRITE_PATH = "../Avatar"
 const CLICK_TO_START_PATH = "../ClickText"
@@ -8,6 +9,7 @@ var clicked = true
 var main_menu
 var avatar
 var click_text
+var button_sfx_player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,13 +17,18 @@ func _ready():
 	avatar = get_node(AVATAR_SPRITE_PATH)
 	click_text = get_node(CLICK_TO_START_PATH)
 	click_text.visible = false
+	button_sfx_player = get_node(BUTTON_SFX_PLAYER_PATH)
 		
 	$VBoxContainer/MenuOptions/BackButton.connect("pressed", self, "_main_menu")
 	$VBoxContainer/MenuOptions/FullscreenButton.connect("pressed", self, "_load_fullscreen")
 	$VBoxContainer/MenuOptions/MusicButton.connect("pressed", self, "_music")
 
+func _play_button_sound():
+	button_sfx_player.play(0.0)
+
 # Goes back to main menu
 func _main_menu():
+	_play_button_sound()
 	self.visible = false
 	click_text.visible = false
 	main_menu.visible = true
@@ -29,13 +36,16 @@ func _main_menu():
 
 # enables/disables fullscreen
 func _load_fullscreen():
+	_play_button_sound()
 	OS.window_fullscreen = !OS.window_fullscreen
 
 # starts stops main music
 func _music():
 	if(clicked):
+		_play_button_sound()
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
 		clicked = false
 	else:
+		_play_button_sound()
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 		clicked = true
